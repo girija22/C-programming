9c438d17d733bca6fa7f9e04bd34893f2b052e83
girija22 <38652911+girija22@users.noreply.github.com>
2018-04-23 09:45:57 +0000

    Create declare.c

---
+++ Beginner level/declare.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ char name[25]="Sachin Ramesh Tendulkar";
+ char nickname[20]="Little Master";
+ int runs=18426;
+ float avg=44.83;
+ int matches=463;
+ printf("%s\n",name);
+ printf("%s\n",nickname);
+ printf("%d\n",runs);
+ printf("%.2f\n",avg);
+ printf("%d",matches);
+ return 0;
+}
+
