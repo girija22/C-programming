935a92225c2db84ed08760772c9bf79542c8a068
girija22 <38652911+girija22@users.noreply.github.com>
2018-04-24 07:13:38 +0000

    Create divmod.c

---
+++ Player level/divmod.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,count=0;
+ printf("Enter two numbers");
+ scanf("%d %d",&a,&b);
+ while((a-b)>=0)
+ {
+  count++;
+  a-=b;
+	
+}
+   printf("%d\n",count);
+    printf("%d",a);
+   return 0;
+ 
+}
