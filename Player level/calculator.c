092106c6f3d72f87ae9a19d4f4cb8710a878de73
girija22 <38652911+girija22@users.noreply.github.com>
2018-04-24 04:44:22 +0000

    Create calculator.c

---
+++ Player level/calculator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int add;
+  int sub;
+  int multiply;
+  int div;
+  int a=10,b=5;
+  add=a+b;
+  sub=a-b;
+  multiply=a*b;
+  div=a/b;
+  printf("%d\n",add);
+  printf("%d\n",sub);
+  printf("%d\n",multiply);
+  printf("%d\n",div);
+  return 0;
+}
