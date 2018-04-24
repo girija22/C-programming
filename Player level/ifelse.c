1a59ed6c7639acc2190de4f5f0e775371dd55360
girija22 <38652911+girija22@users.noreply.github.com>
2018-04-24 05:09:48 +0000

    Create ifelse.c

---
+++ Player level/ifelse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int b;
+printf("Enter a value:");
+scanf("%d", &b);
+if(b>0)
+printf("The value is positive");
+else
+printf("the value is negative");
+return 0;
+}
