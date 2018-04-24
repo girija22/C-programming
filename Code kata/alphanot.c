431e5e3b81e1571efb1b663faf4e3806c2cf9d05
girija22 <38652911+girija22@users.noreply.github.com>
2018-04-24 09:58:48 +0000

    Add files via upload

---
+++ Code kata/alphanot.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+	char z;
+	scanf("%c",&z);
+	if(z>='a'&& z<='z'||z>='A'&& z<='Z')
+	{
+		printf("Alphabet");
+	}
+	else
+	{
+		printf("No");
+	}
+	return 0;
+}
+
