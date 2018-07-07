88ba39fe6f5f974c2d700a340475094136470f0c
girija22 <38652911+girija22@users.noreply.github.com>
2018-07-07 06:36:43 +0000

    Create primein.c

---
+++ Code kata/primein.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n,a,b,i,count;
+    scanf("%d %d", &a, &b);
+    for(i=a+1;i<b;i++)	
+    {
+        count = 0;
+	for(n = 2;n<i;n++)
+        {
+            if(i%n== 0)
+            {
+                count++;
+                break;
+            }
+        }
+
+        if (count == 0)
+            printf("%d ", i);
+
+       
+    }
+
+    return 0;
+}
